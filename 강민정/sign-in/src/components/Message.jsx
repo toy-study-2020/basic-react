0b98e9a78740f0bf50553dac7c1b255a7ca54d1b
minjeongkang <snowdrop1202@gmail.com>
2020-09-26 14:55:09 +0900

    React.js - 회원가입 필수 입력정보 영역 검증 변수로 분리해서 에러 메세지/class 분기

--- 강민정/sign-in/src/components/Message.jsx
+++ 강민정/sign-in/src/components/Message.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {MSG} from '../constants/constants';
 
 const Message = ({ type, value })  => {
+  const clear = value !== '' && value.indexOf('default') < 0;
   return (
     <>
-        {(type !== 'mail' && type !== 'gender') &&
-          <span className="alert">
-            {value !== '' && value.indexOf('default') < 0 ? MSG.CLEAR : MSG.DEFAULT}
+        {(type !== 'mail') &&
+          <span className={clear ? 'clear' : 'alert'}>
+            {clear ? MSG.CLEAR : MSG.DEFAULT}
           </span>
         }
     </>
