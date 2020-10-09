c39fa72d982f071417ec52b29ad459498e11f533
tnwjd2050 <tnwjd2050@naver.com>
2020-10-09 19:53:15 +0900

    라우트 설정 추가

---
+++ 박수정/react-router/src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h2>
+                메인
+            </h2>
+        </div>
+    );
+};
+
+export default Main;
