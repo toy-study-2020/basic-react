9bf8ff1ecf0f574fd8320ff3ae42660c4e87f0be
tnwjd2050 <tnwjd2050@naver.com>
2020-10-09 22:37:08 +0900

    네비 추가

--- 박수정/react-router/src/pages/Main.js
+++ 박수정/react-router/src/pages/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import Nav from '../components/Nav';
 
 const Main = () => {
     return (
         <div>
             <Header/>
+            <section>
+                <Nav/>
+            </section>
         </div>
     );
 };
