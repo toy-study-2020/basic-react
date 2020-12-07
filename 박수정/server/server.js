6b4b9d02d4865ffb3d0e2c220b9b46f6f9fe2507
baekCode <baekcode@gmail.com>
2020-12-07 22:51:42 +0900

    add : server 추가

---
+++ 박수정/server/server.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+server.use(router)
+
+let port = 3030;
+server.listen(port, () => {
+  console.log(`JSON Server is running, port(${port})`)
+})
