6b50c0a3c4eb936f0c751ea6cfadb892391b592b
minjeongkang <snowdrop1202@gmail.com>
2020-12-10 00:12:51 +0900

    json 데이터 CRUD 메소드 레이아웃 설계

---
+++ 강민정/public/scripts/common.js
@@ -0,0 +1,44 @@
+import { URL } from './constants.js';
+
+const FETCH = {
+  getDB: async _ => {
+    const response = await fetch(`${URL}/posts`);
+    if (response.ok) {
+      return await response.json();
+    }
+
+  },
+  postDB: async ({
+    type: type,
+    id: id,
+    title: title,
+    author: author
+  }) => {
+    console.log(type, id, title, author);
+  },
+  updateDB: async ({
+    type: type,
+    id: id,
+    title: title,
+    author: author
+  }) => {
+    console.log(type, id, title, author);
+  },
+  deleteDB: async ({
+    id: id
+  }) => {
+    console.log(id)
+  }
+};
+
+const { getDB, postDB, updateDB, deleteDB } = FETCH;
+
+const init = async _ => {
+  const db = await getDB();
+  await console.log(db);
+}
+if (document.readyState === 'complete') {
+  init();
+} else if (document.addEventListener) {
+  document.addEventListener('DOMContentLoaded', init);
+}
